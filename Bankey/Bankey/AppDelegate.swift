47fcf30d32192a67bb2554a31001510c2658a596
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2022-01-01 00:22:42 +0200

    initial-commit

---
+++ Bankey/Bankey/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Bankey
+//
+//  Created by Ahmad Medhat on 01/01/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .blue
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+
+
+}
+
